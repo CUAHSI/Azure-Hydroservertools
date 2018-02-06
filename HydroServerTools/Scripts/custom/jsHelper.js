ca6c3d635360ef734572c6c28a7890022ce178ab
Martin seul <martinseul@live.com>
2018-02-05 21:02:26 -0500

    added recreate Seriescatalog

---
+++ HydroServerTools/Scripts/custom/jsHelper.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function () {
+    $.ajax({
+        type: "POST",
+        url: "/Home/GetSyncStatus/",
+        success: function (result) {
+            
+            $('#syncText').text(result);
+        },
+        error: function (result) {
+            $('#syncText').text(result);
+        }
+    });
+});
+$("#buttonSyncData").click(function (e) {
+    e.preventDefault();
+    
+    $.ajax({
+        type: "POST",
+        url: "/Home/RecreateSeriescatalog/",
+        success: function (result) {
+            alert('ok');
+        },
+        error: function (result) {
+            alert('error');
+        }
+    });
+});
