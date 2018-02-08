8d65b626c21df263d68d4b1a9e50a22d7ce11043
ANightCoder-Brian <ANightCoder@comcast.net>
2018-02-07 21:57:06 -0500

    - Forgot one file = defining interface: IHydroserverRepositoryProxy from the previous check-in...

---
+++ HydroserverToolsBusinessObjects/Interfaces/IHydroserverRepositoryProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydroserverToolsBusinessObjects.Interfaces
+{
+    //Define a generic interface for: Initializing 'proxy' values from a 'source' instance
+    //                                Setting 'source' values from a 'proxy' instance 
+    public interface IHydroserverRepositoryProxy<tSourceType, tProxyType> where tSourceType : class, new()
+                                                                          where tProxyType : class, new()
+    {
+        tProxyType InitializeProxy();
+        tSourceType ValueFromProxy(tProxyType proxy);
+
+        //Compare values of source and proxy instances - return true on match, false otherwise...
+        bool CompareWithProxy(tProxyType proxy);
+    }
+}
