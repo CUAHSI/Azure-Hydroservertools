985aab2741df0f22f7f5a866c24aff53148785c1
ANightCoder-Brian <ANightCoder.brian@gmail.com>
2018-03-02 16:46:11 -0500

    HydroServer Redesign Part 20
    **** Summary ****
     - Fix for WDC-1558 - Minor Bug Fixes
    
         Fixed by these changes:
          - Ensure 'Finish' and 'Cancel' actions always start a new upload session
            (i.e., remove all client- and server-based temporary items for the current
            uploadId, generate a new uploadId)
    
         Already fixed by previous changes for other issues:
         - Fix previous button navigation issues - especially display of upload results
           for previously uploaded files on File Upload page
    
    **** HydroServerTools ****
     - HydroServerTools.csproj
       - new file: Scripts\custom\revisedUploadUtils.js
    
    **** HydroServerTools/Scripts/custom ****
     - new file: revisedUploadUtils.js
    
    **** HydroServerTools/Views/CSVUpload ****
     - DbSummaryReport.cshtml
       - remove commented out code
       - add <script> tag for revisedUploadUtils.js
       - remove function: removeUploadId(...)
     - DragAndDropFiles.cshtml
       - add <script> tag for revisedUploadUtils.js
       - In Cancel 'anchor' click handler - add call to removeUploadId(...)
     - ValidateFiles.cshtml
       - add <script> tag for revisedUploadUtils.js
       - In Cancel 'anchor' click handler - add call to removeUploadId(...)

---
+++ HydroServerTools/Scripts/custom/revisedUploadUtils.js
@@ -0,0 +1,32 @@
+﻿//
+// A simple file of utility methods for use on the 'Revised Uploader' pages
+
+//Request removal of the uploadId...
+function removeUploadId(currentUploadId) {
+    //Validate/initialize input parameters...
+    if ('undefined' !== typeof currentUploadId && null !== currentUploadId) {
+        //Input parameters valid - set url...
+        var url = '/api/revisedupload/delete/uploadId/' + currentUploadId + '/';
+
+        $.ajax({
+            "url": url,
+            "type": "DELETE",
+            "async": true,
+            "dataType": "json",
+            "cache": false, //So IE does not cache when calling the same URL - source: http://stackoverflow.com/questions/7846707/ie9-jquery-ajax-call-first-time-doing-well-second-time-not
+            "success": function (data, textStatus, jqXHR) {
+                console.log('RevisedUpload DELETE/uploadId success!!');
+
+                //Redirect to home page, after a brief delay...
+                setTimeout(function () {
+                    window.location.href = "/CSVUpload/RevisedUploadData/SelectUploadType"
+                }, 1000);
+            },
+            "error": function (xmlhttprequest, textStatus, message) {
+                //Failure - Log messsage received from server...
+                console.log('RevisedUpload DELETE/uploadId reports error: ' + xmlhttprequest.status + ' (' + message + ')');
+            }
+        });
+    }
+}
+
