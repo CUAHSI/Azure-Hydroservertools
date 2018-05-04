ff7619b16f1f93a5074a315d074db8a6bc56277c
ANightCoder-Brian <ANightCoder.brian@gmail.com>
2018-05-04 13:03:39 -0400

    Changes for WDC Sprint 22 - WDC-1860
    >>> Summary <<<
     - Make output of debug JavaScript console messages conditional on build type:
       - DEBUG - enable debug console message output
       - RELEASE - disable debug console message output
    
    >>>> Azure-Hydroservertools/HydroServerTools <<<<
     - HydroServerTools.csproj
       - added files:
         - Extensions\HtmlHelperExtensions.cs
         - Scripts\custom\ConsoleUtils.js
    
    >>>> Azure-Hydroservertools/HydroServerTools\Extensions <<<<
     - Added file:
        - HtmlHelperExtensions.cs
    
    >>>> Azure-Hydroservertools/HydroServerTools/Scripts/custom <<<<
     - added file: ConsoleUtils.js
    
    >>>> Azure-Hydroservertools/HydroServerTools/Scripts/custom/workers <<<<
     - modified files for conditional JavaScript console messages:
        - workerCsvValidationResults.js
        - workerDbLoadStatus.js
        - workerDbRecordCounts.js
        - workerUploadIdKeepAlive.js
    
    >>>> Azure-Hydroservertools/HydroServerTools/Views/CSVUpload <<<<
     - modified files for conditional JavaScript console messages:
       - DbSummaryReport.cshtml
       - DragAndDropFilesRevised.cshtml
       - SelectUploadType.cshtml
    - Miscellaneous additions of statement terminators ';'
    
     NOTE: Some files contains tabbing/spacing changes of unknown origin

---
+++ HydroServerTools/Scripts/custom/ConsoleUtils.js
@@ -0,0 +1,42 @@
+﻿//
+// A simple file defining some console-related utilities...
+//
+
+//Enable/Disable console functions per the input boolean
+// true: Enable console functions, false: Disable console functions...
+//Sources: https://www.codebyamir.com/blog/suppressing-console-log-messages-in-production
+//         https://stackoverflow.com/questions/1215392/how-to-quickly-and-conveniently-disable-all-console-log-statements-in-my-code
+function EnableDisableConsole(bEnableDisable) {
+    //Validate/initialize input parameters...
+    if ('undefined' !== typeof bEnableDisable && null !== bEnableDisable) {
+        //Input parameters valid - evaluate...
+        var originalProperties = console.originalProperties;
+
+        if (bEnableDisable) {
+            //Enable requested - check if console disabled...
+            if ('undefined' !== typeof originalProperties && null !== originalProperties) {
+                //Console disabled - retrieve original properties
+                for (var eProp in originalProperties) {
+                    console[eProp] = originalProperties[eProp];
+                }
+
+                //Remove original properties...
+                originalProperties = null;
+                delete console.originalProperties;
+            }
+        }
+        else {
+            //Disable requested - check if console enabled...
+            if ('undefined' === typeof originalProperties || null === originalProperties) {
+                //Console enabled - retain/replace original properties...
+                originalProperties = {};
+                for (var dProp in console) {
+                    originalProperties[dProp] = console[dProp];
+                    console[dProp] = function () { };
+                }
+
+                console.originalProperties = originalProperties;
+            }
+        }
+    }
+}
